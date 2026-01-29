b0059c63ed6e610529b08f807973309b599d6ac8
Gunasai Vallu <vallugunasai05@gmail.com>
2026-01-29 16:57:56 +0530

    updated frontend & backend

--- frontend/app.py
+++ frontend/app.py
@@ -5,7 +5,7 @@ import requests
 # ==================================================
 # CONFIG
 # ==================================================
-BACKEND_URL = "http://127.0.0.1:8000/analyze"
+BACKEND_URL = "http://localhost:8000/analyze"
 
 
 st.set_page_config(
